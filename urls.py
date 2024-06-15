116f838b615173a7442b38bbe52113da6c8db349
mo5302 <132563502+mo5302@users.noreply.github.com>
2024-06-15 07:00:20 +0700

    Add files via upload
    Trong folder demo, thêm các phần code vào file settings.py, urls.py cho chức năng user_profile

--- urls.py
+++ urls.py
@@ -1,38 +1,14 @@
-from django.urls import path
-from . import views
-from .views import (
-    ItemDetailView,
-    HomeView,
-    add_to_cart,
-    remove_from_cart,
-    ShopView,
-    OrderSummaryView,
-    remove_single_item_from_cart,
-    CheckoutView,
-    PaymentView,
-    AddCouponView,
-    RequestRefundView,
-    CategoryView,
-    SearchView,
-    ProfileView
-)
-
-app_name = 'core'
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('profile/', ProfileView.as_view(), name='view_profile'),
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('category/<slug>/', CategoryView.as_view(), name='category'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('add_coupon/', AddCouponView.as_view(), name='add-coupon'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('shop/', ShopView.as_view(), name='shop'),
-    path('search', SearchView.as_view(), name='search'),
-    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
-         name='remove-single-item-from-cart'),
-    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
-    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls', namespace='core'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
