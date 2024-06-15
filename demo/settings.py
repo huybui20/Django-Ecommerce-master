148329ebe0be7792339c0d2c97dcab69cd3e75f6
huybui20 <buidamquanghuy20@gmail.com>
2024-06-15 15:25:10 +0700

    email

--- demo/settings.py
+++ demo/settings.py
@@ -119,3 +119,11 @@ STRIPE_SECRET_KEY = 'sk_test_tn0CTDaIJHUJyAqhsf39cfsC00LNjsqDnb'
 #MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+#EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'vuxuanhoa2403@gmail.com'
+EMAIL_HOST_PASSWORD = 'ablvmhzevdohfyqx'
